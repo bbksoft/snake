7827d0a53bf458b7a756c6174419b0cf063c44f8
bbksoft <14985666@qq.com>
2016-07-20 17:04:59 +0800

    gameplay

--- server/aow/lib/base/game.ex
+++ server/aow/lib/base/game.ex
@@ -15,13 +15,13 @@ defmodule Game do
 		GenServer.start(__MODULE__,nil,[name: :g_game])
 	end
 
-    def init({socket,mods}) do
+    def init(_) do
 		state = %{class: :game, actors: %{}, pionts: %{} }
-		state = add_mod(state,mods)
+        send_after(self(), :tick, 100)
 		{:ok, state}
 	end
 
-    defp new_actor()
+    defp new_actor() do
         x = Enum.random(1..100)
         y = Enum.random(1..100)
 
@@ -37,8 +37,8 @@ defmodule Game do
         max_id = state.max_id
         actor = new_actor()
         actors = state.actors
-        state = %{state | max_id: max_id + 1, actors: %actors{max_id->actor} }
-		{:reply,%{user | gameInfo: %{id=max_id,gamePid=self()}}, state}
+        state = %{state | max_id: max_id + 1, actors: %{actors | max_id => actor} }
+		{:reply,%{user | gameInfo: %{id: max_id,gamePid: self()}}, state}
 	end
 
 	def handle_cast({:turn,user,time,dx,dy},state) do
@@ -47,9 +47,40 @@ defmodule Game do
 	end
 
     def handle_info(:tick,state) do
-        state = {state | actor: Enum.map(actors, fn({id,atcor})
+        send_after(self(), :tick, 100)
 
+        state = List.foldl(Map.keys(state.actors), state, fn(s,id) ->
+            actor_update(s,id)
         end)
+    end
+
+    defp actor_update(state,id) do
+
+        actor = state[id]
+
+        coll_pts,new_pts = find_coll_pts(state.points,actor)
+
+
+        actor = List.foldl(coll_pts, actor, fn(pt) ->
+            len_tail = actor.len_tail + 0.2
+            %{actor | len_tail: len_tail}
+        end)
+
+        if actor.len_tail > 0  do
+            actor = %{  actor |
+                        len_tail: (actor.len_tail-0.1),
+                        len: (actor.len+0.1) }
+        end
+
+        if actor_coll(state,actor) do
+            boardcast("death",[id])
+            %{state | id => nil }
+        else
+            %{state | id => actor, points: new_pts }
+        end
+    end
+
+    defp boardcast(cmd,params)
     end
 
 end
--- server/aow/lib/base/tools.ex
+++ server/aow/lib/base/tools.ex
@@ -1,11 +1,11 @@
 
 defmodule Tools do
- 	
+
  	def list_fold_2([a|list1],[b|list2],acc,fun) do
 		acc = fun.(a,b,acc)
 		list_fold_2(list1,list2,acc,fun)
 	end
-	
+
 	def list_fold_2(_,_,acc,_) do
 		acc
 	end
@@ -14,19 +14,19 @@ defmodule Tools do
 		acc = fun.(a,b,c,acc)
 		list_fold_3(list1,list2,list3,acc,fun)
 	end
-	
+
 	def list_fold_3(_,_,_,acc,_) do
 		acc
 	end
-	
 
-	defmacro log(term) do		
+
+	defmacro log(term) do
 		quote do
 			list = String.split(__ENV__.file,"/")
 			file_name = List.last(list)
-			IO.puts "[file:" <> to_string(file_name) 
-				<> " line:" 
-				<> to_string(__ENV__.line) <> "] " 
+			IO.puts "[file:" <> to_string(file_name)
+				<> " line:"
+				<> to_string(__ENV__.line) <> "] "
 				<> Macro.to_string(unquote(term))
 		end
 	end
@@ -35,9 +35,9 @@ defmodule Tools do
 		quote do
 			list = String.split(__ENV__.file,"/")
 			file_name = List.last(list)
-			IO.puts "[error -> file:" <> to_string(file_name) 
-				<> " line:" 
-				<> to_string(__ENV__.line) <> "] " 
+			IO.puts "[error -> file:" <> to_string(file_name)
+				<> " line:"
+				<> to_string(__ENV__.line) <> "] "
 				<> Macro.to_string(unquote(term))
 		end
 	end
@@ -46,11 +46,25 @@ defmodule Tools do
 		quote do
 			list = String.split(__ENV__.file,"/")
 			file_name = List.last(list)
-			IO.puts "[debug -> file:" <> to_string(file_name) 
-				<> " line:" 
-				<> to_string(__ENV__.line) <> "] " 
+			IO.puts "[debug -> file:" <> to_string(file_name)
+				<> " line:"
+				<> to_string(__ENV__.line) <> "] "
 				<> Macro.to_string(unquote(term))
 		end
 	end
+
+    def dis(pos1,pos2) do
+        dx = pos1.x - pos2.x
+        dy = pos1.y - pos2.y
+
+        Math.sqrt(dx*dx+dy*dy)
+    end
+
+    def dis2(pos1,pos2) do
+        dx = pos1.x - pos2.x
+        dy = pos1.y - pos2.y
+
+        dx*dx+dy*dy
+    end
 
 end
